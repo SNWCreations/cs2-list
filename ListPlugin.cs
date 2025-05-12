8170e1bfe2be03d3ed1b8b8c635295a21c07db23
ZX夏夜之风 <snwcreations@qq.com>
2025-05-12 22:00:26 +0800

    init: initial commit

---
+++ ListPlugin.cs
@@ -0,0 +1,24 @@
+﻿using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Core.Attributes.Registration;
+using CounterStrikeSharp.API.Modules.Commands;
+
+namespace cs2_list;
+
+public class ListPlugin : BasePlugin
+{
+    public override string ModuleName => "list";
+    public override string ModuleVersion => "1.0.0";
+    public override string ModuleAuthor => "SNWCreations";
+    public override string ModuleDescription => "Add 'list' command to list players in the server.";
+
+    [ConsoleCommand("css_list", "List the online players (excluding bots)")]
+    [CommandHelper(whoCanExecute: CommandUsage.CLIENT_AND_SERVER)]
+    public void OnCmdList(CCSPlayerController? caller, CommandInfo info)
+    {
+        var players = Utilities.GetPlayers().FindAll(it => it is { IsBot: false });
+        var nameConcat = string.Join(", ", players.ConvertAll(it => it.PlayerName));
+        var response = "There are " + players.Count + " players online: " + nameConcat;
+        info.ReplyToCommand(response);
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# cs2-list
+
+Add 'list' command to list players in the server.
+
+Use `css_list` if you are using console, or `/list` if you are in-game.
+
+## License
+
+Copyright (C) 2025 SNWCreations, Licensed under MIT License.
